7ee36e8870e085e5a628f6565bea8df8009e2d75
therealgodwin <godwinlaureto.dev17@gmail.com>
2021-07-11 18:10:41 +0800

    added firebase.

---
+++ email.php
@@ -0,0 +1,12 @@
+
+<?php
+
+if($_POST["message"], $_POST["name"]) {
+
+mail("godwinlaureto.dev17@gmail.com", "Personal website feedback",
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+}
+
+?>
